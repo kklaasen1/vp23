3a1ea43a7bf859b434be0f3300bb8418aa4c2c21
Karmen Klaasen <kklaasen1@users.noreply.github.com>
2023-09-28 17:28:39 +0300

    Tund 4

--- www1.js
+++ www1.js
@@ -1,11 +1,33 @@
 const http = require("http");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
 
 http.createServer(function(req, res){
-	res.writeHead(200, {"Content-type": "text/html"});
-	res.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Karmen Klaasen, veebiprogrammeerimine 2023</title></head><body>');
-	res.write('<h1>Karmen Klaasen</h1><p>See veebileht on valminud <a href="https://www.tlu.ee" target="_blank">TLÜ</a> Digitehnoloogiate instituudi informaatika eriala õppetöö raames.</p>');
-	res.write('<hr></body></html>');
-	console.log("Keegi vaatab");
+	let currentURL = url.parse(req.url, true);
+	console.log(currentURL);
+	if (currentURL.pathname === "/"){
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Karmen Klaasen, veebiprogrammeerimine 2023</title></head><body>');
+		res.write('<img src="banner.png" alt="Kursuse bänner">');
+		res.write('<h1>Karmen Klaasen</h1><p>See veebileht on valminud <a href="https://www.tlu.ee" target="_blank">TLÜ</a> Digitehnoloogiate instituudi informaatika eriala õppetöö raames.</p>');
+		res.write('<hr></body></html>');
+		console.log("Keegi vaatab");
+	}
+	else if (currentURL.pathname === "/banner.png"){
+		console.log("Tahame pilti!");
+		let bannerPath = path.join(__dirname, "public", "banner");
+		
+		fs.readFile(bannerPath + currentURL.pathname, (err, data)=>{
+			if (err) {
+				throw err;
+			}
+			else {
+				res.writeHead(200, {"Content-type": "image/png"});
+				res.end(data);
+			}
+		});
+	}
 	//valmis, saada ära
 	return res.end();
 }).listen(5122);
